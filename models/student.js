8fcc6d2ea39217c791d257300e57882e9c264dec
Oscar Brown <hello@oscarbrown.com>
2014-08-01 11:57:11 -0500

    get actions

--- models/student.js
+++ models/student.js
@@ -11,7 +11,21 @@ var student = Backbone.Model.extend({
     
     
     checkInOut: function(parentID) {
-
+		
+		var actions = (_.isUndefined(this.get('actions')) ? new Array() : this.get('actions');
+		
+		var whatHappened = (this.get('checkedIn')) ? 'check out' : 'check in';
+		
+		var newAction = {
+			roomID: app.students.classroomID,
+			parentID: parentID,
+			action: whatHappened,
+			time: (new Date).getTime()
+		};
+		
+		actions.push(newAction);
+		
+		this.set('actions', actions)
 	    this.set('checkInParentID', parentID);
 	    this.set('checkedIn', !this.get('checkedIn'));
 	    this.save();
